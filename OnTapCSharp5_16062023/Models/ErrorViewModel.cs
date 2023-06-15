b03f0216cf11597dfe722eabe004ed80af552919
Trung27012003 <trung27012003@gmail.com>
2023-06-16 00:23:08 +0700

    Add project files.

---
+++ OnTapCSharp5_16062023/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnTapCSharp5_16062023.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
